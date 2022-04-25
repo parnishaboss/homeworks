291cdb2f790ed3f11cb312048cd1a883e7dca8a7
parnishaboss <zhora0@mail.ru>
2022-04-25 16:24:29 +0300

    second homework

--- src/p2-homeworks/h2/HW2.tsx
+++ src/p2-homeworks/h2/HW2.tsx
@@ -1,13 +1,15 @@
 import React, {useState} from 'react'
 import Affairs from './Affairs'
 
-// types
-export type AffairPriorityType = any // need to fix any
-export type AffairType = any // need to fix any
+export type AffairPriorityType = 'high' | 'low' | 'middle'
+export type AffairType = {
+    _id: number
+    name: string
+    priority: AffairPriorityType
+}
 export type FilterType = 'all' | AffairPriorityType
 
-// constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: AffairType[] = [
     {_id: 1, name: 'React', priority: 'high'},
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -15,21 +17,20 @@ const defaultAffairs: any = [ // need to fix any
     {_id: 5, name: 'html & css', priority: 'middle'},
 ]
 
-// pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
+export const filterAffairs = (affairs: AffairType[], filter: FilterType):AffairType[] => {
     if (filter === 'all') return affairs
-    else return // need to fix
+    else return affairs.filter(a => a.priority === filter)
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-    return // need to fix
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => {
+    return affairs.filter(a => a._id !== _id)
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs)
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => setAffairs(deleteAffair(affairs, _id)) // need to fix any
+    const deleteAffairCallback = (_id: number) => setAffairs(deleteAffair(affairs, _id))
 
     return (
         <div>
