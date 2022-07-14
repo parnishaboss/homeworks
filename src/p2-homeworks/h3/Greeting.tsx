927d6f326b9439c0d3917252171c331dba708765
parnishaboss <zhora0@mail.ru>
2022-07-14 22:37:48 +0300

    third homework (not finished)

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,25 @@
-import React from 'react'
+import {ChangeEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
-
+    // const inputClass = error ? s.errorInput : s.input // need to fix with (?:)
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
+            <input value={name} onChange={setNameCallback} className={error ? s.errorInput : ''}/>
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
+            {error && <div className={s.errorMessage}>{error}</div>}
         </div>
     )
 }
